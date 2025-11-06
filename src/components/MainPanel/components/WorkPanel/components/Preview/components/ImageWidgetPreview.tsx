d6271903c92c9708d9c832d425b828d19c9f2976
Volodymyr <volus1986@gmail.com>
2025-11-06 14:30:28 +0200

    fix image position

--- src/components/MainPanel/components/WorkPanel/components/Preview/components/ImageWidgetPreview.tsx
+++ src/components/MainPanel/components/WorkPanel/components/Preview/components/ImageWidgetPreview.tsx
@@ -1,13 +1,7 @@
 export default function ParagraphWidgetPreview({ value }: { value: string }) {
     return (
-        <img
-            className="
-                max-w-full px-[15px] rounded-sm
-                font-[Roboto] font-normal text-[14px] leading-[150%] tracking-[0.275px]
-                align-middle text-center
-            "
-            src={value}
-            alt={value}
-        />
+        <div className="w-full px-[15px]">
+            <img className="max-w-full rounded-sm" src={value} alt={value} />
+        </div>
     );
 }
