aba027482c1f577a0071af6d4d0be957c514d124
Volodymyr <volus1986@gmail.com>
2025-11-06 14:24:27 +0200

    code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import MainPanel from './components/MainPanel';
 function App() {
     return (
         <div className="w-full h-full grid justify-center items-center bg-gray-300">
-            <div className="w-360 grid grid-rows-[auto_1fr] h-212 rounded-2xl overflow-hidden bg-[#e4e6f1]">
+            <div className="w-[1440px] grid grid-rows-[auto_1fr] h-[848px] rounded-2xl overflow-hidden bg-[#e4e6f1]">
                 <TopPanel />
                 <MainPanel />
             </div>
